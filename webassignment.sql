a483ebcdb9fb97c644a53e01579e75ffb37d83dc
Steven Wang <Steven.Wang@interniti.efinitiservices.com>
2017-07-21 13:37:24 +1000

    commit for upload work to github

---
+++ webassignment.sql
@@ -0,0 +1,48 @@
+
+
+
+
+
+--
+-- Create schema Webassignment
+--
+
+CREATE DATABASE IF NOT EXISTS webassignment;
+USE webassignment;
+
+--
+-- Definition of table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` varchar(15) NOT NULL primary key,
+  `password` varchar(15) NOT NULL,
+  `role` varchar(15) NOT NULL
+);
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(15) NOT NULL,
+  `customer` varchar(20) NOT NULL,
+  `address` varchar(15) NOT NULL,
+  `totalprice` decimal(10,2) NOT NULL,
+  `shippingcost` decimal(10,2) NOT NULL,
+  `finalcost` decimal(10,2) NOT NULL,
+  `cart` varchar(50000) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+--
+-- Dumping data for table `users`
+--
+
+
+INSERT INTO `users` (`username`,`password`,`role`) VALUES 
+ ('ge','wang','admin'),
+ ('test','test','regular');
+
+
+
+
+
